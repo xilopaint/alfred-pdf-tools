b7bfaa0653645d828844992e3c51da4316b6896d
xilopaint <arthurpinheiro@me.com>
2016-07-23 20:28:06 -0300

    Ability to set the output filename when merging files

--- src/merge.py
+++ src/merge.py
@@ -4,14 +4,15 @@
 import sys
 from PyPDF2 import PdfFileMerger, PdfFileReader
 from send2trash import send2trash
-from os.path import expanduser
+import os.path
 import ntpath
 
 
 def main():
 
-    query = sys.argv[1:]
-    files = [x for x in query if x.endswith('.pdf')]
+    abs_paths = os.environ['abs_paths']
+    query = os.environ['query']
+    files = abs_paths.split('\t')
     paths = [get_path(path) for path in files]
     merger = PdfFileMerger()
 
@@ -35,7 +36,7 @@ def main():
         for pdf in files:
             merger.append(PdfFileReader(open(pdf, 'rb')))
 
-        merger.write(paths[0] + "/output.pdf")
+        merger.write(paths[0] + "/" + query + ".pdf")
 
         for pdf in files:
             send2trash(pdf)
