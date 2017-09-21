e39a38068dfa14227f1b7e15847f18ec6cb705e4
xilopaint <arthurpinheiro@me.com>
2017-09-20 23:42:35 -0300

    New auto update feature
    – Removed support for OneUpdater. From now Alfred PDF Tools relies on
    Alfred-Workflow library for self-updating.
    – Significant code improvements for better stability and maintenance.

--- README.md
+++ README.md
@@ -17,8 +17,10 @@ Download the workflow file from [GitHub releases][2] and install it by double-cl
 * `Optimize`: Optimize the selected PDF files by entering the intended resolution of the output file (150 dpi is set once no value is input). The document will be improved on the process with increased contrast and text straightening. Invoke Alfred and type the keyword `progress` if you want to track the optimization process;
 * `Encrypt`: Encrypt the selected PDF files by entering a password;
 * `Merge`: Merge the selected PDF files. Use the `⌘` modifier key if you also want to move the source files to Trash;
-* `Split`: Split the selected PDF file either by page count or file size (use the `⌘` modifier key in the latter case);
-* `Slice`:  Slice the selected PDF file in multiple files by entering page numbers and/or page ranges separated by commas (e.g. 2, 5-8). For a single file/slice use the `⌘` modifier key.
+* `Split by Page Count`: Split the selected PDF file by page count.
+* `Split by File Size`: Split the selected PDF file by file size.
+* `Slice in Multiple Files`: Slice the selected PDF file in multiple files by entering page numbers and/or page ranges separated by commas (e.g. 2, 5-8).
+* `Slice in a Single File`: Slice the selected PDF file in a single file by entering page numbers and/or page ranges separated by commas (e.g. 2, 5-8).
 
 ## Contribute
 
@@ -26,7 +28,7 @@ To report a bug or request a feature, please [create an issue][3] or [submit a p
 
 ## Credits
 
-This workflow relies on [PyPDF2][5] library currently maintained by [Phaseit, Inc.][6], [Alfred-Workflow][7] library by Dean Jackson, [K2pdfopt][8] by Johannes Buchnerand, [Send2Trash][9] by Virgil Dupras and [OneUpdater][10] by Vítor Galvão.
+This workflow relies on [PyPDF2][5] library currently maintained by [Phaseit, Inc.][6], [Alfred-Workflow][7] library by Dean Jackson, [K2pdfopt][8] by Johannes Buchnerand, [Send2Trash][9] by Virgil Dupras and [docopt][10].
 
 ## License
 
@@ -41,5 +43,5 @@ This workflow relies on [PyPDF2][5] library currently maintained by [Phaseit, In
 [7]:https://github.com/deanishe/alfred-workflow
 [8]:http://www.willus.com/k2pdfopt/
 [9]:https://github.com/hsoft/send2trash
-[10]:https://github.com/vitorgalvao/alfred-workflows/tree/master/OneUpdater
+[10]:https://github.com/docopt/docopt
 [11]:https://opensource.org/licenses/MIT
---
+++ src/alfred_pdf_tools.py
@@ -0,0 +1,482 @@
+#!/usr/bin/env python
+# encoding: utf-8
+#
+# Copyright © 2017 Arthur Pinheiro
+#
+# MIT Licence. See http://opensource.org/licenses/MIT
+
+"""
+Optimize, encrypt and manipulate PDF files.
+
+Usage:
+    alfred_pdf_tools.py optimize <query>
+    alfred_pdf_tools.py progress
+    alfred_pdf_tools.py encrypt <query>
+    alfred_pdf_tools.py mrg <query>
+    alfred_pdf_tools.py mrgtrash <query>
+    alfred_pdf_tools.py splitcount <query>
+    alfred_pdf_tools.py splitsize <query>
+    alfred_pdf_tools.py slicemulti <query>
+    alfred_pdf_tools.py slicesingle <query>
+
+Commands:
+    optimize <query>      Optimize PDF files.
+    progress              Track optimization progress.
+    encrypt <query>       Encrypt PDF files.
+    mrg <query>           Merge PDF files.
+    mrgtrash <query>      Merge PDF files and move them to trash.
+    splitcount <query>    Split PDF file by page count.
+    splitsize <query>     Split PDF file by file size.
+    slicemulti <query>    Multi-slice PDF files.
+    slicesingle <query>   Single-slice PDF files.
+"""
+from __future__ import division
+import sys
+import os
+from docopt import docopt
+from workflow import Workflow3, notify, ICON_WARNING
+from subprocess import Popen, PIPE
+from PyPDF2 import PdfFileMerger, PdfFileReader, PdfFileWriter
+from send2trash import send2trash
+
+
+UPDATE_SETTINGS = {'github_slug': 'xilopaint/alfred-pdf-tools'}
+
+
+class AlfredPdfToolsError(Exception):
+    """Creates Subclass of Exception class."""
+    pass
+
+
+class NotIntegerError(AlfredPdfToolsError):
+    """Raised when the user input is not an integer."""
+    pass
+
+
+class NegativeValueError(AlfredPdfToolsError):
+    """Raised when the user input is a negative integer."""
+    pass
+
+
+class SelectionError(AlfredPdfToolsError):
+    """Raised when the user selects less than two PDF files."""
+    pass
+
+
+class MultiplePathsError(AlfredPdfToolsError):
+    """Raised when the user selects PDF files from diferent dirnames."""
+    pass
+
+
+class SyntaxError(AlfredPdfToolsError):
+    """Raised when the input is not a valid syntax."""
+    pass
+
+
+class IndexError(AlfredPdfToolsError):
+    """Raised when the inputted page number is out of range."""
+    pass
+
+
+class StartValueZeroError(AlfredPdfToolsError):
+    """Raise when the inputted page number is zero."""
+    pass
+
+
+class StartValueReverseError(AlfredPdfToolsError):
+    """Raised when a page range is set in reverse order."""
+    pass
+
+
+def optimize(query, n):
+    """Optimize PDF files."""
+    try:
+
+        if query:
+            if not query.lstrip('+-').isdigit():
+                raise NotIntegerError
+
+        if query:
+            if int(query) < 0:
+                raise NegativeValueError
+
+        for i in xrange(n):
+            command = "echo -y | ./k2pdfopt '{}' -as -mode copy -dpi {} -o '%s (optimized).pdf' -x".format(f[i], query)
+            proc = Popen(command, shell=True, stdout=PIPE)
+
+            while proc.poll() is None:
+                line = proc.stdout.readline()
+                if "Reading" in line:
+                    pg_cnt = line.split()
+                    wf.cache_data('page_count', pg_cnt[1])
+
+                if "SOURCE PAGE" in line:
+                    pg_no = line.split()
+                    wf.cache_data('page_number', pg_no[2])
+
+            notify.notify('Alfred PDF Tools',
+                          'Optimization successfully completed.')
+
+    except NotIntegerError:
+        notify.notify('Alfred PDF Tools',
+                      'The argument is not an integer.')
+
+    except NegativeValueError:
+        notify.notify('Alfred PDF Tools',
+                      'Negative integer is not a valid argument.')
+
+
+def get_progress():
+    """Show optimization progress."""
+    pg_no = wf.cached_data('page_number', max_age=10)
+    pg_cnt = wf.cached_data('page_count', max_age=0)
+
+    wf.rerun = 1
+
+    if not pg_no:
+
+        if wf.cached_data_age('page_count') < 10:
+            title = "Reading the PDF file..."
+            wf.add_item(valid=True, title=title)
+        else:
+            title = "Optimize action is not running."
+            wf.add_item(valid=True, title=title, icon=ICON_WARNING)
+
+    else:
+
+        prog_int = int(round((float(pg_no) / float(pg_cnt)) * 100))
+        prog = str(prog_int)
+
+        if pg_no != pg_cnt:
+            title = "Page {} of {} processed ({}% completed)".format(pg_no,
+                                                                     pg_cnt,
+                                                                     prog)
+            wf.add_item(valid=True, title=title)
+
+        else:
+            title = "Page {} of {} processed ({}% completed)".format(pg_no,
+                                                                     pg_cnt,
+                                                                     prog)
+            wf.add_item(valid=True, title=title, icon='checkmark.png')
+
+    return wf.send_feedback()
+
+
+def encrypt(query, abs_path):
+    """Encrypt PDF files."""
+    noextpath = [os.path.splitext(x) for x in f]
+    file_count = len(f)
+
+    for n in xrange(file_count):
+
+        inp_file = open(f[n], 'rb')
+        reader = PdfFileReader(inp_file, strict=False)
+        writer = PdfFileWriter()
+
+        for pg_no in range(reader.numPages):
+            writer.addPage(reader.getPage(pg_no))
+
+        writer.encrypt(query)
+        out_file = open(noextpath[n][0] + ' (encrypted).pdf', 'wb')
+        writer.write(out_file)
+
+    notify.notify('Alfred PDF Tools', 'Encryption successfully completed.')
+
+
+def merge(query, f, trash):
+    """Merge PDF files."""
+    paths = [os.path.split(path)[0] for path in f]
+    merger = PdfFileMerger(strict=False)
+
+    try:
+
+        if len(f) < 2:
+            raise SelectionError
+
+        if not paths[1:] == paths[:-1]:
+            raise MultiplePathsError
+
+        for pdf in f:
+            reader = PdfFileReader(open(pdf, 'rb'))
+            merger.append(reader)
+
+        if trash:
+            for pdf in f:
+                send2trash(pdf)
+
+        merger.write(paths[0] + "/" + query + ".pdf")
+
+    except SelectionError:
+        notify.notify('Alfred PDF Tools',
+                      'You must select at least two PDF files to merge.')
+
+    except MultiplePathsError:
+        notify.notify('Alfred PDF Tools',
+                      'Cannot merge PDF files from multiple paths.')
+
+
+def split_count(query, abs_path):
+    """Split PDF file by page count"""
+    try:
+
+        if not query.lstrip("+-").isdigit():
+            raise NotIntegerError
+
+        if int(query) < 0:
+            raise NegativeValueError
+
+        page_count = int(query)
+        start = 0
+        stop = page_count
+        inp_file = open(abs_path, 'rb')
+        reader = PdfFileReader(inp_file)
+        num_pages = int(reader.getNumPages())
+        quotient = num_pages / page_count
+
+        if quotient.is_integer():
+
+            for i in xrange(int(quotient)):
+                merger = PdfFileMerger(strict=False)
+                merger.append(inp_file, pages=(start, stop))
+                noextpath = os.path.splitext(abs_path)[0]
+                merger.write(noextpath + (' (part {}).pdf').format(i + 1))
+                start = stop
+                stop = start + page_count
+
+        else:
+
+            for i in xrange(int(quotient) + 1):
+                merger = PdfFileMerger(strict=False)
+                merger.append(inp_file, pages=(start, stop))
+                noextpath = os.path.splitext(abs_path)[0]
+                merger.write(noextpath + (' (part {}).pdf').format(i + 1))
+                if i != int(quotient) - 1:
+                    start = stop
+                    stop = start + page_count
+                else:
+                    start = int(quotient) * page_count
+                    stop = num_pages
+
+    except NotIntegerError:
+        notify.notify('Alfred PDF Tools', 'The argument is not an integer.')
+
+    except NegativeValueError:
+        notify.notify('Alfred PDF Tools',
+                      'Negative integer is not a valid argument.')
+
+    except ZeroDivisionError:
+        notify.notify('Zero is not a valid argument.')
+
+
+def split_size(query, abs_path):
+    """Split PDF file by file size."""
+    try:
+
+        arg_file_size = float(query) * 1000000
+        noextpath = os.path.splitext(abs_path)[0]
+        inp_file = open(abs_path, 'rb')
+        reader = PdfFileReader(inp_file)
+        pg_cnt = int(reader.getNumPages())
+        start = 0
+        stop = 1
+        pg_no = 0
+
+        while start < pg_cnt:
+            merger = PdfFileMerger(strict=False)
+            merger.append(inp_file, pages=(start, stop))
+            merger.write(noextpath)
+            obj_path = noextpath
+            obj = PdfFileReader(open(obj_path, 'rb'))
+            num_pages_obj = int(obj.getNumPages())
+            file_size = os.path.getsize(obj_path)
+            os.remove(obj_path)
+
+            if file_size < arg_file_size:
+                if stop == pg_cnt:
+                    merger = PdfFileMerger(strict=False)
+                    merger.append(inp_file, pages=(start, stop))
+                    merger.write(noextpath +
+                                 (' (part {}).pdf').format(pg_no + 1))
+                    break
+                else:
+                    stop = stop + 1
+
+            else:
+                if num_pages_obj == 1:
+                    merger = PdfFileMerger(strict=False)
+                    merger.append(inp_file, pages=(start, stop))
+                    merger.write(noextpath +
+                                 (' (part {}).pdf').format(pg_no + 1))
+                    start = stop
+                    stop = stop + 1
+                    pg_no = pg_no + 1
+
+                else:
+                    stop = stop - 1
+                    merger = PdfFileMerger(strict=False)
+                    merger.append(inp_file, pages=(start, stop))
+                    merger.write(noextpath +
+                                 (' (part {}).pdf').format(pg_no + 1))
+                    start = stop
+                    stop = stop + 1
+                    pg_no = pg_no + 1
+
+    except ValueError:
+        notify.notify('Alfred PDF Tools',
+                      'The argument must be a positive numeric value.')
+
+
+def slice_(query, abs_path, single):
+    """Slice PDF files."""
+    try:
+
+        pages = [x.strip() for x in query.split(',')]
+        page = [x.split('-') for x in pages]
+        inp_file = open(abs_path, 'rb')
+        reader = PdfFileReader(inp_file)
+        pg_cnt = reader.getNumPages()
+
+        for n in xrange(len(pages)):
+
+            if pages[n].replace('-', '').isdigit():
+                pass
+
+            else:
+                raise SyntaxError
+
+        for n in xrange(len(pages)):
+
+            if "-" in pages[n]:
+                stop = int(page[n][1])
+                if stop > pg_cnt:
+                    raise IndexError
+                else:
+                    pass
+
+            else:
+                stop = int(pages[n])
+                if stop > pg_cnt:
+                    raise IndexError
+                else:
+                    pass
+
+        noextpath = os.path.splitext(abs_path)[0]
+
+        if single:
+
+            merger = PdfFileMerger(strict=False)
+            inp_file = open(abs_path, 'rb')
+            reader = PdfFileReader(inp_file)
+
+            for n in xrange(len(pages)):
+
+                if "-" in pages[n]:
+                    start = int(page[n][0]) - 1
+                    stop = int(page[n][1])
+
+                    if start == -1:
+                        raise StartValueZeroError
+
+                    if start >= stop:
+                        raise StartValueReverseError
+
+                    merger.append(reader, pages=(start, stop))
+
+                else:
+
+                    start = int(pages[n]) - 1
+                    stop = int(pages[n])
+                    merger.append(reader, pages=(start, stop))
+
+            merger.write(noextpath + ' (slice).pdf')
+
+        else:
+
+            for n in xrange(len(pages)):
+
+                if "-" in pages[n]:
+                    merger = PdfFileMerger(strict=False)
+                    inp_file = open(abs_path, 'rb')
+                    reader = PdfFileReader(inp_file)
+                    start = int(page[n][0]) - 1
+                    stop = int(page[n][1])
+
+                    if start == -1:
+                        raise StartValueZeroError
+
+                    if start >= stop:
+                        raise StartValueReverseError
+
+                    merger.append(reader, pages=(start, stop))
+                    merger.write(noextpath + (' (part {}).pdf').format(n + 1))
+
+                else:
+                    merger = PdfFileMerger(strict=False)
+                    reader = PdfFileReader(open(abs_path, 'rb'))
+                    start = int(pages[n]) - 1
+                    stop = int(pages[n])
+                    merger.append(reader, pages=(start, stop))
+                    merger.write(noextpath + (' (part {}).pdf').format(n + 1))
+
+    except SyntaxError:
+        notify.notify('Alfred PDF Tools', 'The input syntax is not valid.')
+
+    except IndexError:
+        notify.notify('Alfred PDF Tools', 'Page number out of range.')
+
+    except StartValueZeroError:
+        notify.notify('Alfred PDF Tools', 'Page number cannot be zero.')
+
+    except StartValueReverseError:
+        notify.notify('Alfred PDF Tools',
+                      'You cannot set a page range in reverse order.')
+
+
+def main(wf):
+    """Run workflow."""
+    global f
+
+    args = docopt(__doc__, wf.args)
+
+    query = os.environ['query']
+    abs_path = os.environ['abs_path']
+    f = abs_path.split('\t')
+    n = len(f)
+
+    if args.get('optimize'):
+        optimize(query, n)
+
+    elif args.get('progress'):
+        get_progress()
+
+    elif args.get('encrypt'):
+        encrypt(query, abs_path)
+
+    elif args.get('mrg'):
+        merge(query, f, False)
+
+    elif args.get('mrgtrash'):
+        merge(query, f, True)
+
+    elif args.get('splitcount'):
+        split_count(query, abs_path)
+
+    elif args.get('splitsize'):
+        split_size(query, abs_path)
+
+    elif args.get('slicemulti'):
+        slice_(query, abs_path, False)
+
+    else:
+        slice_(query, abs_path, True)
+
+    if wf.update_available:
+        notify.notify('Alfred PDF Tools',
+                      'A newer version of the workflow is available.',
+                      'Glass')
+        wf.start_update()
+
+
+if __name__ == '__main__':
+    wf = Workflow3(update_settings=UPDATE_SETTINGS)
+    sys.exit(wf.run(main))
